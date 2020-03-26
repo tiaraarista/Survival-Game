fed6110d8673bb1fb35bb0dbbd8ce5447e625155
tiaraarista <tiaraarista99@gmail.com>
2020-03-27 03:49:04 +0700

    Pert5

---
+++ Assets/Script/EnemyMovement.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+	CharacterController _controller;
+	Transform _player;
+
+	[SerializeField]
+	float _moveSpeed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		GameObject playerObject = GameObject.FindGameObjectWithTag ("Player");
+		_player = playerObject.transform;
+		_controller = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = _player.position - transform.position;
+
+		direction.Normalize ();
+		Vector3 velocity = direction * _moveSpeed;
+		_controller.Move (velocity * Time.deltaTime);
+	}
+}
